6180d44fc28664d52efb82498e04516854612cf8
boltercio <juanantonio.detena@gmail.com>
2025-12-06 21:31:25 +0100

    Invertir la lógica de verificación de paquetes en el script de instalación

--- autobspwm.sh
+++ autobspwm.sh
@@ -51,7 +51,7 @@ unzip fzf xsel imagenmagick acpi locate build-essential)
 
 for paquete in "${paquetes[@]}"; do
     consulta=$(dpkg-query -s $paquete &>/dev/null)
-    if [ "$consulta" ]; then
+    if [ ! "$consulta" ]; then
         sudo apt-get install -qq -y "$paquete" &>/dev/null
         if [ "$(echo $?)" != 0 ]; then
             echo -e "${red}[!]${gray} La instalacion de ${paquete} ha fallado.${end}"
