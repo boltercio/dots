a4a030f8328044775cab42f825071eab08aa52a9
boltercio <juanantonio.detena@gmail.com>
2026-02-25 17:23:38 +0100

    Actualizado lista de paquetes para contenedor

--- Linux/ct-platform.sh
+++ Linux/ct-platform.sh
@@ -113,7 +113,14 @@ echo -e "${green}[+]${gray} Eliminando repositorio descargado.${end}"
 cd $HOME
 rm -rf dots
 
+# deshabilitando configuracion automatica de red
+echo -e "${green}[+]${gray} Parando configuracion automatica de red.${end}"
+systemctl stop systemd-networkd &>/dev/null
+systemctl disable systemd-networkd &>/dev/null
+systemctl mask systemd-networkd &>/dev/null
+
 # Instalando cliente wazuh
+echo -e "${green}[+]${gray} Instalando agente de monitoreo wazuh.${end}"
 wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.14.3-1_arm64.deb && sudo WAZUH_MANAGER='192.168.0.100' WAZUH_AGENT_NAME="$(hostname)" dpkg -i ./wazuh-agent_4.14.3-1_arm64.deb
 
 # Cambiando shell por defecto
---
+++ README.md
@@ -0,0 +1,7 @@
+Instalacion en contedor 
+
+```
+curl -LO https://raw.githubusercontent.com/boltercio/dots/refs/heads/main/Linux/ct-platform.sh
+chmod +x ct-platform.
+./ct-platform
+```
