b3fcbe46e1eb208cf065d9d9915e134daa504872
boltercio <juanantonio.detena@gmail.com>
2026-04-03 14:00:51 +0200

    mas actualizaciones de lo mismo

--- Linux/ct-platform.sh
+++ Linux/ct-platform.sh
@@ -59,7 +59,7 @@ function package_install() {
 
 logo
 echo -e "${green}[+]${gray} Instalando paquetes necesarios...${end}"
-paquetes=(zsh lsd bat git wget unzip lsb-release)
+paquetes=(zsh lsd bat git wget unzip lsb-release python3)
 
 for paquete in "${paquetes[@]}"; do
     package_install $paquete
