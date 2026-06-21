63f5f563fe877c7c080db6068259a562c934d8cb
boltercio <juanantonio.detena@gmail.com>
2026-06-21 09:04:06 +0000

    modificada instalacion firefox

--- autobspwm.sh
+++ autobspwm.sh
@@ -96,16 +96,16 @@ sudo install -d -m 0755 /etc/apt/keyrings > /dev/null
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O - | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list > /dev/null
 echo '
-Package: firefox
+Package: firefox thunderbird
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
 
-Package: firefox*
+Package: firefox* thunderbird*
 Pin: release o=Ubuntu
 Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox > /dev/null
 sudo apt update &>/dev/null
-package_install "firefox"
+package_install firefox
 
 # copiar directorios de configuracion
 echo -e "${green}[+]${gray} Copiando configuraciones.${end}"
